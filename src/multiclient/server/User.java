472edb16dc8bf0c0ab8a1d99fc28e4b3718baf49
Johannes Strige <s236652@stud.hioa.no>
2017-02-23 17:17:00 +0100

    Created userclass

---
+++ src/multiclient/server/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiclient.server;
+
+/**
+ *
+ * @author Johannes
+ */
+public class User {
+    private String password;
+    private String username;
+}
